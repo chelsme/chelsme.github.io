1787b862ee340a77492666b871c91f5e578d9121
Chelsea Crowson <chelsea.crowson@umbrage.com>
2024-06-28 12:14:57 -0500

    reverse project order

--- src/data.js
+++ src/data.js
@@ -44,68 +44,109 @@ export const experienceData = [
 
 export const projectData = [
   {
-    description: "Proof of Concept for internal Accenture effort. Assisted in creation of functioning front-end application built on locally created mock data.",
-    technologies: ["Angular"],
-    period: "08/2019 - 11/2019"
+    "description": "Data analytics app. Platform for tracking sales and creating reports using data to display tables and charts showcasing report data.",
+    "technologies": [
+      "NestJS",
+      "GraphQL",
+      "Angular",
+      "RXJS"
+    ],
+    "period": "08/2023 - 06/2024"
   },
   {
-    description: "Worked on a university project involving Ruby on Rails.",
-    technologies: ["Ruby on Rails"],
-    period: "12/2019 - 01/2020"
+    "description": "Developed an iPad app for tracking tasks and assets.",
+    "technologies": [
+      "SwiftUI"
+    ],
+    "period": "06/2023 - 08/2023"
   },
   {
-    description: "Front-end developer on major system redesign for an internal use application. Built and standardized reusable components to be shared throughout the application and for use by other development teams.",
-    technologies: ["React"],
-    period: "03/2020 - 05/2020"
+    "description": "Backend Tech Lead and Backend developer for large scale internal client dashboard.",
+    "technologies": [
+      "NodeJS",
+      "Docker",
+      "TypeOrm",
+      "NestJS"
+    ],
+    "period": "01/2023 - 05/2023"
   },
   {
-    description: "Full-stack developer on a custom client-facing Alexa Skill for a major insurance company.",
-    technologies: ["Alexa Skills Kit", "Alexa Design Language", "Node.js"],
-    period: "01/2020 - 11/2020"
+    "description": "Tech Lead and Backend developer for customer-facing iOS app regarding sports betting and fantasy sports.",
+    "technologies": [
+      "NodeJS",
+      "GraphQL",
+      "SQL",
+      "Express"
+    ],
+    "period": "02/2023 - 12/2023"
   },
   {
-    description: "Co-lead a team to refactor an internal application to incorporate reusable elements. Mentored junior developers on best practices of reusable components.",
-    technologies: ["React", "Flux"],
-    period: "11/2020"
+    "description": "Front-end developer on Oil and Gas Analytics app. Created reusable D3 charts and graphs with variable axes.",
+    "technologies": [
+      "React",
+      "D3",
+      "Ag-Grid"
+    ],
+    "period": "10/2021 - 02/2022"
   },
   {
-    description: "Front-end developer and mentor on multi-part microservices redesign. Mentored junior developers on best practices.",
-    technologies: ["React", "Redux"],
-    period: "11/2020 - 07/2021"
+    "description": "Developed an iOS app for task and asset tracking.",
+    "technologies": [
+      "Swift"
+    ],
+    "period": "09/2021 - 10/2021"
   },
   {
-    description: "Co-owned 24 pages of global site redesign. Authored reusable components to be utilized throughout the global restructure of the webpage.",
-    technologies: ["IBM WebSphere Application Server"],
-    period: "07/2021 - 09/2021"
+    "description": "Co-owned 24 pages of global site redesign. Authored reusable components to be utilized throughout the global restructure of the webpage.",
+    "technologies": [
+      "IBM WebSphere Application Server"
+    ],
+    "period": "07/2021 - 09/2021"
   },
   {
-    description: "Developed an iOS app for task and asset tracking.",
-    technologies: ["Swift"],
-    period: "09/2021 - 10/2021"
+    "description": "Front-end developer and mentor on multi-part microservices redesign. Mentored junior developers on best practices.",
+    "technologies": [
+      "React",
+      "Redux"
+    ],
+    "period": "11/2020 - 07/2021"
   },
   {
-    description: "Front-end developer on Oil and Gas Analytics app. Created reusable D3 charts and graphs with variable axes.",
-    technologies: ["React", "D3", "Ag-Grid"],
-    period: "10/2021 - 02/2022"
+    "description": "Co-lead a team to refactor an internal application to incorporate reusable elements. Mentored junior developers on best practices of reusable components.",
+    "technologies": [
+      "React",
+      "Flux"
+    ],
+    "period": "11/2020"
   },
   {
-    description: "Tech Lead and Backend developer for customer-facing iOS app regarding sports betting and fantasy sports.",
-    technologies: ["NodeJS", "GraphQL", "SQL", "Express"],
-    period: "02/2023 - 12/2023"
+    "description": "Full-stack developer on a custom client-facing Alexa Skill for a major insurance company.",
+    "technologies": [
+      "Alexa Skills Kit",
+      "Alexa Design Language",
+      "Node.js"
+    ],
+    "period": "01/2020 - 11/2020"
   },
   {
-    description: "Backend Tech Lead and Backend developer for large scale internal client dashboard.",
-    technologies: ["NodeJS", "Docker", "TypeOrm", "NestJS"],
-    period: "01/2023 - 05/2023"
+    "description": "Front-end developer on major system redesign for an internal use application. Built and standardized reusable components to be shared throughout the application and for use by other development teams.",
+    "technologies": [
+      "React"
+    ],
+    "period": "03/2020 - 05/2020"
   },
   {
-    description: "Developed an iPad app for tracking tasks and assets.",
-    technologies: ["SwiftUI"],
-    period: "06/2023 - 08/2023"
+    "description": "Worked on a university project involving Ruby on Rails.",
+    "technologies": [
+      "Ruby on Rails"
+    ],
+    "period": "12/2019 - 01/2020"
   },
   {
-    description: "Data analytics app. Platform for tracking sales and creating reports using data to display tables and charts showcasing report data.",
-    technologies: ["NestJS", "GraphQL", "Angular", "RXJS"],
-    period: "08/2023 - 06/2024"
+    "description": "Proof of Concept for internal Accenture effort. Assisted in creation of functioning front-end application built on locally created mock data.",
+    "technologies": [
+      "Angular"
+    ],
+    "period": "08/2019 - 11/2019"
   }
 ];
